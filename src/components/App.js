de0c5c93cc73f7712cac787efe779290ad9ba030
Aditya Kumar <106439535+AddySoulja@users.noreply.github.com>
2023-01-11 17:24:54 +0000

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,16 @@ import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
 //code here
+const [inputValue, setInputValue] = useState("");
+const [value, setValue] = useState("");
+const getInputValue = () => {
+  setValue(inputValue)
+}
   return (
     <div id="main">
-      <input id='input' onChange={}></input>
-      <button id='button' onClick={}>Click</button>
-      <p id='text'> Hello my name is ____ and I study at Newton School</p>
+      <input id='input' onChange={(e) => setInputValue(e.target.value)}></input>
+      <button id='button' onClick={getInputValue}>Click</button>
+      <p id='text'> Hello my name is {value === "" ? "____" : value} and I study at Newton School</p>
     </div>
   )
 }
